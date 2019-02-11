5094f9df64a20db32f86922f3ca3d8daaa291f50
weifengan <weifengan@163.com>
2019-02-11 18:02:52 +0800

    初始化项目文件

---
+++ xLuaFramework/Assets/Scripts/Demo.cs
@@ -0,0 +1,28 @@
+﻿/*************************************
+			
+		Demo 
+		
+   @desction:
+   @author:felixwee
+   @email:felixwee@163.com
+   @website:www.felixwee.com
+  
+***************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class Demo : MonoBehaviour {
+
+    LuaEnv lua = new LuaEnv();
+	// Use this for initialization
+	void Start () {
+        lua.DoString("print(\"hello\")");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
